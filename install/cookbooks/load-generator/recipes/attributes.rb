2fa1ab9c0895942a65396a9134167e550870cfc7
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-01-24 13:49:54 +0100

    Fix chef-solo incompatible set recipe

--- install/cookbooks/load-generator/recipes/attributes.rb
+++ install/cookbooks/load-generator/recipes/attributes.rb
@@ -1,7 +1,7 @@
 extend Opscode::OpenSSL::Password
 
-node.set_unless['load-generator']['db']['password'] = secure_password
-node.set_unless['load-generator']['env']['SECRET_KEY_BASE'] = secure_password
-node.set['apt']['compile_time_update'] = true
-node.set['build-essential']['compile_time'] = true
-node.save
+# node.set_unless['load-generator']['db']['password'] = secure_password
+# node.set_unless['load-generator']['env']['SECRET_KEY_BASE'] = secure_password
+node.default['apt']['compile_time_update'] = true
+node.default['build-essential']['compile_time'] = true
+# node.save
