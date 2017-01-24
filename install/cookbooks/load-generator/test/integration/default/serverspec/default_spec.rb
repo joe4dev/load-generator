78133cfe5ad18cd7e7f0121aa8d1dc1321332246
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-01-24 13:46:02 +0100

    Add basic integration tests

--- install/cookbooks/load-generator/test/integration/default/serverspec/default_spec.rb
+++ install/cookbooks/load-generator/test/integration/default/serverspec/default_spec.rb
@@ -1,12 +1,26 @@
 require 'spec_helper'
 
+# Serverspec examples can be found at
+# http://serverspec.org/resource_types.html
 describe 'load-generator::default' do
-
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  describe user('deploy') do
+    it { should exist }
+    it { should have_home_directory '/home/deploy' }
   end
 
+  it "is listening on port 80" do
+    expect(port(80)).to be_listening
+  end
+
+  it "has a running service of mysql-default" do
+    expect(service("postgresql")).to be_running
+  end
+
+  it "has a running service of apache2" do
+    expect(service("nginx")).to be_running
+  end
+
+  describe command('wget http://192.168.33.44 -O - -q') do
+    its(:stdout) { should contain('Listing Jmeter Tasks') }
+  end
 end
